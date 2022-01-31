89cf9df1d94011844d5237ef5cbbab665be4045b
heavensbee001 <heavensbee001@gmail.com>
2022-01-31 16:04:59 +0100

    Add some quicktest scripts

--- scripts/run.js
+++ scripts/run.js
@@ -6,6 +6,57 @@ const main = async () => {
 
 	console.log('Contract deployed to:', wagmiTeamsContract.address)
 	console.log('Contract deployed by:', owner.address)
+
+	// quickTest
+	let allHackathonPositions
+	allHackathonPositions = await wagmiTeamsContract.getPaginatedPositions(0, 1, 10)
+	console.log('allHackathonPositions:', allHackathonPositions)
+
+	let allJobPositions
+	allJobPositions = await wagmiTeamsContract.getPaginatedPositions(1, 1, 10)
+	console.log('allJobPositions:', allJobPositions)
+
+	await wagmiTeamsContract.sendPosition(0, {
+		title: 'Title',
+		projectOrCompanyName: 'Hackathon name',
+		projectOrCompanyImageUrl: 'imgUrl',
+		description: 'Hackathon description',
+		positionOfferUrl: 'url1',
+		contact: 'my twitter 1',
+		createdAt: 123421341234,
+	})
+	allHackathonPositions = await wagmiTeamsContract.getPaginatedPositions(0, 1, 10)
+	console.log('allHackathonPositions:', allHackathonPositions)
+
+	await wagmiTeamsContract.sendPosition(1, {
+		title: 'Title',
+		projectOrCompanyName: 'Job name',
+		projectOrCompanyImageUrl: 'imgUrl',
+		description: 'Job description',
+		positionOfferUrl: 'url2',
+		contact: 'my twitter 2',
+		createdAt: 123421341235,
+	})
+	await wagmiTeamsContract.sendPosition(1, {
+		title: 'Title',
+		projectOrCompanyName: 'Job name',
+		projectOrCompanyImageUrl: 'imgUrl',
+		description: 'Job description',
+		positionOfferUrl: 'url2',
+		contact: 'my twitter 2',
+		createdAt: 123421341235,
+	})
+	await wagmiTeamsContract.sendPosition(1, {
+		title: 'Title',
+		projectOrCompanyName: 'Job name',
+		projectOrCompanyImageUrl: 'imgUrl',
+		description: 'Job description',
+		positionOfferUrl: 'url2',
+		contact: 'my twitter 2',
+		createdAt: 123421341235,
+	})
+	allJobPositions = await wagmiTeamsContract.getPaginatedPositions(1, 1, 10)
+	console.log('allJobPositions:', allJobPositions)
 }
 
 const runMain = async () => {
